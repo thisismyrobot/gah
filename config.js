1bf51ca85be3e16b704f49f7fd1af432365a1299
Robert Wallhead <rwallhead@gmail.com>
2016-09-24 13:11:12 +1000

    Config example

---
+++ config.js
@@ -0,0 +1,13 @@
+// You must define VIEW_ID, CLIENT_ID and TARGET for this example to work.
+
+// View IDs under
+// https://analytics.google.com/analytics/web/#management/Settings in your
+// analytics account - there is usually (as a default) one view per site you
+// have configured.
+var VIEW_ID = '';
+
+// OAuth Client ID - 
+var CLIENT_ID = '';
+
+// Target is the path you are interested in.
+var TARGET = '/';
