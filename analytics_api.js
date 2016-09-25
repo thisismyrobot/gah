102018474040d93eb941a7fe344a0642bfd2e73d
Robert Wallhead <rwallhead@gmail.com>
2016-09-25 13:06:20 +1000

    Moved the Analystics API hook into the example

--- analytics_api.js
+++
@@ -1,8 +0,0 @@
-// Bring in the API client.
-// From https://developers.google.com/analytics/devguides/reporting/embed/v1/getting-started#step-2
-(function(w,d,s,g,js,fjs){
-    g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(cb){this.q.push(cb)}};
-    js=d.createElement(s);fjs=d.getElementsByTagName(s)[0];
-    js.src='https://apis.google.com/js/platform.js';
-    fjs.parentNode.insertBefore(js,fjs);js.onload=function(){g.load('analytics')};
-}(window,document,'script'));
